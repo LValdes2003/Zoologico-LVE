a0153f0006f2877bf35689788e0b4e0754944e00
LValdes2003 <lvaldesp@myuax.com>
2024-03-06 22:26:23 +0100

    tests y main

---
+++ src/test/java/HabitatAcuaticoTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HabitatAcuaticoTest {
+
+    @Test
+    void getSalinidad() {
+    }
+
+    @Test
+    void setSalinidad() {
+    }
+}
---
+++ src/test/java/HabitatTest.java
@@ -0,0 +1,38 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HabitatTest {
+
+    @Test
+    void getTemperatura() {
+    }
+
+    @Test
+    void setTemperatura() {
+    }
+
+    @Test
+    void getHumedad() {
+    }
+
+    @Test
+    void setHumedad() {
+    }
+
+    @Test
+    void isLimpieza() {
+    }
+
+    @Test
+    void setLimpieza() {
+    }
+
+    @Test
+    void getAnimales() {
+    }
+
+    @Test
+    void setAnimales() {
+    }
+}
